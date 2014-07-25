78b472c6c4fa38ebedc654815b7d4724c3851f22
Chintan Choksi <chintan@Chintans-MacBook-Pro.local>
2014-07-25 14:36:32 -0400

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'experimentmanager',
+  packages = [''], 
+  version = '0.1',
+  description = "Seattle's experimentmanager package",
+  author = 'Seattle',
+  url = 'https://github.com/BuildSeash/experimentmanager', # use the URL to the github repo
+  classifiers = [],
+)
